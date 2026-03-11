39d61ddbfcbc2634604c1dafb8d6b241b737e5c6
Purvasha Padhy <purvasha1013@gmail.com>
2026-03-11 19:36:04 +1100

    Add health insurance providers to database
    Inserted major and mid-sized Australian health insurance providers into the database.

---
+++ Database/insurance_provider.sql
@@ -0,0 +1,18 @@
+-- Insert major and mid-sized Australian health insurance providers
+INSERT INTO insurance_providers (name, website) VALUES
+('Medibank', 'https://www.medibank.com.au'),
+('Bupa Australia', 'https://www.bupa.com.au'),
+('HCF', 'https://www.hcf.com.au'),
+('HBF', 'https://www.hbf.com.au'),
+('nib', 'https://www.nib.com.au'),
+('ahm Health Insurance', 'https://www.ahm.com.au'),
+('Australian Unity', 'https://www.australianunity.com.au'),
+('GMHBA', 'https://www.gmhba.com.au'),
+('Latrobe Health Services', 'https://www.latrobehealth.com.au'),
+('HIF Health Insurance', 'https://www.hif.com.au'),
+('Health Partners', 'https://www.healthpartners.com.au'),
+('Westfund Health Insurance', 'https://www.westfund.com.au'),
+('Peoplecare Health Insurance', 'https://www.peoplecare.com.au'),
+('Phoenix Health Fund', 'https://www.phoenixhealthfund.com.au'),
+('St.LukesHealth', 'https://www.stlukes.com.au'),
+('Mildura Health Fund', 'https://www.mildurahealthfund.com.au');
